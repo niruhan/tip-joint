69baacec496978715f59ced9429a6a1807990203
niruhan <niruhanv@gmail.com>
2022-10-13 14:21:44 -0400

    Add fixed gaussian psi filtering

--- denoise_mat_vec_image.m
+++ denoise_mat_vec_image.m
@@ -1,64 +1,49 @@
+clc
+clear 
+close all
+
 % Read noisy lena image
 noisy_img = imread("img/noisy_lena.png");
-% figure,
-% imshow(noisy_img);
+figure,
+imshow(noisy_img);
 
 % generate matrix o double type with the image values
 [h, w] = size(noisy_img);
 noisy_img_matrix = im2double(noisy_img);
 filter_output = zeros(h, w);
 
-hw = 1;
-a = hw;
-b = hw;
-
-% define a 9x9 filter
-row1 = [1,1,0,1,1,0,0,0,0];
-row2 = [1,1,1,1,1,1,0,0,0];
-row3 = [0,1,1,0,1,1,0,0,0];
-row4 = [1,1,0,1,1,0,1,1,0];
-row5 = [1,1,1,1,1,1,1,1,1];
-row6 = [0,1,1,0,1,1,0,1,1];
-row7 = [0,0,0,1,1,0,1,1,0];
-row8 = [0,0,0,1,1,1,1,1,1];
-row9 = [0,0,0,0,1,1,0,1,1];
-
-g = [row1;row2;row3;row4;row5;row6;row7;row8;row9];
+kernel_halfwidth = 1;
+kernel_size = 2 * kernel_halfwidth + 1;
+patch_height = 10;
+patch_width = 10;
+% 
+g = gaussianFilter(kernel_size, 2);
+% 
+psi = preparePsi(g, patch_height, patch_width, kernel_halfwidth);
 
 % run sinhorn knopp normalization on g
-g_normalized = sinkhornKnopp(g);
+psi_normalized = sinkhornKnopp(psi);
 
-for row = 1:h/3
+for row = 1:h/patch_height
     % find the vertical coordinates of the patch
-    patch_start_vertical = (row - 1) * 3 + 1;
-    patch_end_vertical = (row - 1) * 3 + 3;
-    for col = 1: w/3
+    patch_start_vertical = (row - 1) * patch_height + 1;
+    patch_end_vertical = row * patch_height;
+    for col = 1: w/patch_width
         % find the end coordinates of the patch
-        patch_start_horizontal = (col - 1) * 3 + 1;
-        patch_end_horizontal = (col - 1) * 3 + 3;
-
-        disp(patch_start_vertical)
-        disp(patch_start_horizontal)
-        disp(patch_end_vertical)
-        disp(patch_end_horizontal)
+        patch_start_horizontal = (col - 1) * patch_width + 1;
+        patch_end_horizontal = col * patch_width;
 
         noisy_img_patch = noisy_img_matrix(patch_start_vertical: patch_end_vertical, patch_start_horizontal: patch_end_horizontal);
-%         imwrite(mat2gray(noisy_img_patch), 'img/noisy_img_patch.png');
-        flattened_patch = reshape(noisy_img_patch.',1,[]);
+        flattened_patch = reshape(noisy_img_patch.',[],1);
 
-        flat_output = g_normalized * flattened_patch';
-        matrix_output = [flattened_patch(1:3); flattened_patch(4:6); flattened_patch(7:9)];
-        filter_output(patch_start_vertical: patch_end_vertical, patch_start_horizontal: patch_end_horizontal) = matrix_output;
+        flat_output = psi_normalized * flattened_patch;
+        matrix_output = reshape(flat_output, patch_height, patch_width);
+        filter_output(patch_start_vertical: patch_end_vertical, patch_start_horizontal: patch_end_horizontal) = matrix_output';
     end
 end
-
-% show the noisy patch as image
-% figure,
-% patch_image = mat2gray(noisy_img_patch);
-% imshow(patch_image);
 
 % generate image from the filtered output
 figure,
 filtered_image = mat2gray(filter_output);
 imshow(filtered_image);
-imwrite(filtered_image, 'img/oct-11-mat-vec-denoised.png');
+imwrite(filtered_image, 'img/oct-13-mat-vec-denoised.png');
--- gs.m
+++
@@ -1,4 +0,0 @@
-function out = gs(p,q,var)
-%     d = size(p);
-    out = 1/((2*pi)*var^2)*exp(-(p-q)*(p-q)'/(2*var^2));
-    return
